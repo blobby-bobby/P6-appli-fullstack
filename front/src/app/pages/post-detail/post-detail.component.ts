7913c0ffb1d37cdc7dca002cbcf41f1d3ceb0e61
blobby-bobby <bulle.ouvrard@gmail.com>
2024-07-22 21:33:19 +0200

    feat(wip): post a comment section

--- front/src/app/pages/post-detail/post-detail.component.ts
+++ front/src/app/pages/post-detail/post-detail.component.ts
@@ -4,11 +4,23 @@ import { ActivatedRoute } from '@angular/router';
 import { PostsService } from '../../services/posts.service';
 import { NgFor, NgIf } from '@angular/common';
 import { NavLayoutComponent } from '../../layouts/nav-layout/nav-layout.component';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-post-detail',
   standalone: true,
-  imports: [NavLayoutComponent, NgIf, NgFor],
+  imports: [
+    NavLayoutComponent,
+    NgIf,
+    NgFor,
+    MatFormField,
+    MatInput,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './post-detail.component.html',
   styleUrl: './post-detail.component.css',
 })
