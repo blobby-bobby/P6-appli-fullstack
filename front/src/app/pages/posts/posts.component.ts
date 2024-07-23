f7c3c85946fac22ed5e0a2d1512751be2354fa0d
blobby-bobby <bulle.ouvrard@gmail.com>
2024-07-23 19:23:35 +0200

    make select work to sort by date/title

--- front/src/app/pages/posts/posts.component.ts
+++ front/src/app/pages/posts/posts.component.ts
@@ -4,6 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { NavLayoutComponent } from '../../layouts/nav-layout/nav-layout.component';
 import { PostListComponent } from '../../components/post-list/post-list.component';
+import { Post } from '../../../types/Post';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-posts',
@@ -18,4 +20,23 @@ import { PostListComponent } from '../../components/post-list/post-list.componen
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.css',
 })
-export class PostsComponent {}
+export class PostsComponent {
+  posts: Post[] = [];
+
+  constructor(private postsService: PostsService) {}
+
+  ngOnInit(): void {
+    this.posts = this.postsService.getAllPosts();
+  }
+
+  sortPosts(sortBy: string) {
+    if (sortBy === 'date') {
+      this.posts = this.posts.sort(
+        (a, b) =>
+          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+      );
+    } else if (sortBy === 'title') {
+      this.posts = this.posts.sort((a, b) => a.title.localeCompare(b.title));
+    }
+  }
+}
