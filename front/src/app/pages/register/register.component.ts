20e9c8538aa4a67548c325f3cb3d0f8d55616c73
blobby-bobby <bulle.ouvrard@gmail.com>
2024-07-24 01:02:07 +0200

    fix: update angular material styles

--- front/src/app/pages/register/register.component.ts
+++ front/src/app/pages/register/register.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { AuthLayoutComponent } from '../../layouts/auth-layout/auth-layout.component';
 import { RouterLink } from '@angular/router';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [AuthLayoutComponent, RouterLink],
+  imports: [
+    AuthLayoutComponent,
+    RouterLink,
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
