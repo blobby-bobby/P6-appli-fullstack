b4d1dea9f6b0cd2b5c550ed649c2857277e052a5
blobby-bobby <bulle.ouvrard@gmail.com>
2024-07-24 13:37:25 +0200

    fix: removed unused values

--- resources/script.sql
+++ resources/script.sql
@@ -2,8 +2,6 @@ CREATE TABLE `USERS` (
   `id` integer PRIMARY KEY AUTO_INCREMENT,
   `email` varchar(255),
   `username` varchar(255),
-  `last_name` varchar(255),
-  `first_name` varchar(255),
   `password` varchar(255),
   `created_at` timestamp,
   `updated_at` timestamp
