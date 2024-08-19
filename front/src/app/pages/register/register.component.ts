59d46d40d64636dbe5c65aae96cc5d0625b2204a
blobby-bobby <bulle.ouvrard@gmail.com>
2024-08-19 20:28:51 +0200

    feat(wip): setting up registration

--- front/src/app/pages/register/register.component.ts
+++ front/src/app/pages/register/register.component.ts
@@ -3,7 +3,16 @@ import { AuthLayoutComponent } from '../../layouts/auth-layout/auth-layout.compo
 import { RouterLink } from '@angular/router';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
@@ -16,8 +25,43 @@ import { MatInputModule } from '@angular/material/input';
     MatButtonModule,
     FormsModule,
     MatInputModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  matchingPasswordsValidator: ValidatorFn = (
+    control: AbstractControl
+  ): ValidationErrors | null => {
+    const password = control.get('password');
+    const confirmPassword = control.get('passwordConfirm');
+    return password &&
+      confirmPassword &&
+      password.value !== confirmPassword.value
+      ? { notMatching: true }
+      : null;
+  };
+
+  registerForm = new FormGroup(
+    {
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,60}$'),
+      ]),
+      passwordConfirm: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,60}$'),
+      ]),
+    },
+    { validators: this.matchingPasswordsValidator }
+  );
+
+  public onSubmit(): void {}
+}
--- front/src/app/services/auth.service.ts
+++ front/src/app/services/auth.service.ts
@@ -1,16 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RegisterRequest } from '../../interfaces/RegisterRequest.interface';
+import { AuthSuccess } from '../../interfaces/AuthSuccess.interface';
+
+const baseUrl = 'http://localhost:3002/api';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   login(): void {
     // TO DO
   }
 
-  register(): void {
+  register(registerRequest: RegisterRequest): Observable<AuthSuccess> {
+    return this.http.post<AuthSuccess>(
+      `${baseUrl}/auth/register`,
+      registerRequest
+    );
+  }
+
+  update(): void {
+    // TO DO
+  }
+
+  me(): void {
     // TO DO
   }
 
