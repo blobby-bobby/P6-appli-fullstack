ee4c3dc52814e84c455cc93a5fa3778efa605dde
blobby-bobby <bulle.ouvrard@gmail.com>
2024-09-05 13:35:16 +0200

    feat: set profile icon and styles

--- front/src/app/components/navbar/navbar.component.ts
+++ front/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, NgIf, NgClass],
+  imports: [RouterLink, RouterLinkActive, NgIf, NgClass, MatIcon],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
